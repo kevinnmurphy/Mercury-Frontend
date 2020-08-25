360d8ee1feabee070cd0897c29893ce6879614e4
qu1xs <kevinnealmurphy@gmail.com>
2020-08-24 20:19:40 -0400

    build out locations class and adapter

--- src/location.js
+++ src/location.js
@@ -1,4 +1,3 @@
-
 class Location {
     static all = []
 
@@ -17,41 +16,98 @@ class Location {
         Location.all.push(this)
     }
 
-    handleClick = (e) => {
 
+    addEventListeners() {
+        this.element.addEventListener('click', this.handleClick)
     }
-    
-    createDeleteButton(el) {
-        let deleteButton = document.createElement('button')
-        deleteButton.textContent = 'Delete'
-        // deleteButton.addEventListener('click', deleteLocation)
-        el.appendChild(deleteButton)
+
+    static findById(id) {
+        return Trip.all.find((trip) => trip.id == id)
+    }
+
+    handleClick = (e) => {
+        if (e.target.className === 'delete') {
+            locationAdapter.deleteLocation(this.id)
+            this.element.remove()
+        } else if (e.target.className === 'update') {
+            e.target.className = "save"
+            e.target.innerText = "Save"
+            this.addUpdateLocationFields(this.id)
+        } else if (e.target.className === 'save') {
+            e.target.className = "update"
+            e.target.innerText = "Update"
+            locationAdapter.patchLocation(this.id)
+        }
+    }
+
+    updatelocationOnDom({name, lat, lon}) {
+        this.name = name
+        this.lat = lat
+        this.lon = lon
+
+        let locationElement = document.querySelector(`#location-${this.id}`)
+
+        locationElement.querySelector('.title').innerText = name
+        locationElement.querySelector('.lat').innerText = lat
+        locationElement.querySelector('.lon').innerText = lon
+    }
+
+    addUpdateLocationFields() {
+        const updateForm = 
+        `
+        <input type="text" name="name" id="location-name" value="${this.name}"<br>
+        <input type="text" name="description" id="location-description" value="${this.lat}"><br>
+        <input type="text" name="description" id="location-description" value="${this.lon}"><br>
+        `
+
+        const formDiv = document.createElement('div')
+        formDiv.id = `update-form-${this.id}`
+        formDiv.innerHTML = updateForm
+        this.element.append(formDiv)
     }
 
 
     fullRender() {
-        this.element.innerHTML = `<h3>${this.name}</h3>`
-        // this.createDeleteButton(this)
+        const title = document.createElement('h3')
+        title.innerText = this.name
+        title.classList.add('title')
+        const latitude = document.createElement('span')
+        latitude.innerText = this.lat
+        latitude.classList.add('lat')
+        const longitude = document.createElement('span')
+        longitude.innerText = this.lon
+        longitude.classList.add('lon')
+
+        const deleteBtn = document.createElement('button')
+        deleteBtn.classList.add('delete')
+        deleteBtn.dataset.id = this.id
+        deleteBtn.innerText = "Delete"
+
+        const updateBtn = document.createElement('button')
+        updateBtn.classList.add('update')
+        updateBtn.dataset.id = this.id
+        updateBtn.innerText = 'Update'
+
+        this.element.appendChild(title)
+        this.element.appendChild(lat)
+        this.element.appendChild(lon)
+
+        this.element.appendChild(deleteBtn)
+        this.element.appendChild(updateBtn)
         
-        // add location button
-        // locationBtn = document.createElement('button')
-        // locationBtn.classList.
-        // this.element.appendChild()
-        // <button id="new-trip-btn">Add Trip</button>
+        locationBtn.addEventListener("click", () => {
+            locationFormConatainer.classList.toggle('d-none')
+        })
+
         return this.element
     }
 
-    locations() {
-        return this.locations.all.filter((location) => location.trip_id == this.id)
-    }
-
-
     attachToDom() {
-        this.tripCollection.append(this.fullRender())
+        this.locationCollection.append(this.fullRender())
         this.addEventListeners()
     }
 
     addEventListeners() {
-        // this.element.addEventListeners('click', this.displayLocations
+        // this.element.addEventListeners('click', this.displayLocations)
     }
 }
--- src/locationAdapter.js
+++ src/locationAdapter.js
@@ -4,102 +4,80 @@ class LocationAdapter{
     }
 
     fetchLocations() {
-        fetch(this.baseUrl)
+        fetch(`${baseUrl}/locations`)
             .then(res => res.json())
             .then(json => {
                 json.data.forEach((location) => this.sanitizeAndAdd(location))
             })
             .catch((err) => console.log(err.message))
     }
-
-    createLocation(location) {
-        const config = {
+    
+    createLocation(locationObj) {
+        let config = {
             method: 'POST',
-            body: JSON.stringify(location),
+            body: JSON.stringify(locationObj),
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             }
         }
-        fetch(this.baseUrl, config)
+        fetch(`${baseUrl}/locations`, config)
             .then(res => res.json())
-            .then(json => this.sanitizeAndAdd(json))
+            .then(json => {
+                return this.sanitizeAndAdd(json.data)
+            })
             .catch((err) => console.log(err.message))
     }
+      
+    patchLocation(locationId) {
+        const form = document.querySelector(`#update-form-${locationId}`)
+        const name = form.querySelector('#location-name').value
+        const description = form.querySelector('#location-description').value
+        
+        let locationObj = {
+            name,
+            description
+        }
 
-    patchLocation(id) {
-        const config = {
+        let config = {
             method: 'PATCH',
-            body: JSON.stringify(location),
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
-            }
+            },
+            body: JSON.stringify(locationObj)
         }
 
-        fetch(this.baseUrl + id, config)
+        fetch(`${baseUrl}/locations/${locationId}`, config)
             .then(res => res.json())
-            .then(json => this.updateDom(json.data))
+            .then(json => {
+                
+                let locationNew  = Location.all.find((i) => i.id == json.data.id)
+                locationNew.updateLocationOnDom({...json.data.attributes}) 
+            })
             .catch((err) => console.log(err.message))
 
-        const form = document.querySelector(`#update-form-${locationId}`)
         form.remove()
     }
 
     deleteLocation(id) {
-        const config = {
+        let config = {
             method: 'DELETE',
-            body: JSON.stringify(location.id),
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             }
         }
 
-        fetch(this.baseUrl + id, config)
+        fetch(`${baseUrl}/locations/${id}`, config)
             .then(res => res.json())
-            .then(json => e.target.parentNode.remove())
+            .then(json => alert(json.message))
             .catch((err) => console.log(err.message))
     }
 
     sanitizeAndAdd(res) {
-        let location = new Location({id: res.id, ...res.attributes})
+        let location = new Location({id: res.id, ...res.attributes, trip_id: res.relationships.trip.data.id})
         location.attachToDom()
     }
-
-    updateDom(locationData) {
-        const location = Location.findById(locationData.id)
-        location.name = locationData.attributes.name
-        location.description = locationData.attributes.description
-        location.fullRender()
-    }
-
-    // attachToDom() {
-    //     const tripCollection = document.querySelector('#trip-collection')
-    //     const tripList = document.querySelector('#trip-list')
-
-    //     const tripDiv = document.createElement('div')
-
-    //     tripDiv.classList.add('trip')
-    //     tripDiv.setAttribute()
-    //     tripCollection.appendChild(tripDiv)
-
-    //     let tripName = document.createElement('h2')
-    //     tripDiv.appendChild(tripName)
-    //     tripName.innerText = `${trip.name}`
-
-
-    //     // let locationBtn = document.createElement("button");
-    //     // locationBtn.classList.add('location-btn');
-    //     // locationBtn.innerHTML = "Add Location";
-    //     // tripDiv.appendChild(locationBtn);
-
-    //     // locationBtn.addEventListener("click", () => {
-    //     //     locationFormConatainer.classList.toggle('d-none')
-    //     // })
-
-    // }
-
-
 
 }
